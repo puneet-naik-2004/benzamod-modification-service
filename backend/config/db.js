968679a64e667b5ba5b55d716e71c024899f4a29
puneet-naik-2004 <naikpuneet09@gmail.com>
2025-09-12 12:46:50 +0530

    added remote url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mydb", {
+    await mongoose.connect(`${process.env.MONGODB_STRING}benzamod`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(" MongoDB connected");
+    console.log(" MongoDB connected",`${process.env.MONGODB_STRING}benzamod`);
   } catch (error) {
     console.error(" MongoDB connection failed:", error);
     process.exit(1);
